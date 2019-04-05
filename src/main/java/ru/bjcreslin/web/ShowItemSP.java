edc0a8347e4d067de6ec9e208075a7c64243d95d
bjcreslin <bjcreslin@gmail.com>
2019-04-05 12:46:05 +0700

    Переделан парсинг СП

--- src/main/java/ru/bjcreslin/web/ShowItemSP.java
+++ src/main/java/ru/bjcreslin/web/ShowItemSP.java
@@ -9,7 +9,7 @@ import ru.bjcreslin.service.ItemSPService;
 @Controller
 public class ShowItemSP {
     @Autowired
-    ItemSPService itemSPService;
+    private ItemSPService itemSPService;
 
     @GetMapping("/showallsp")
     public String action(Model model) {
