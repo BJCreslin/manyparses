28aa7702294ec7305db8d93efba190f7574519fc
bjcreslin <bjcreslin@gmail.com>
2019-04-09 15:31:58 +0700

    Переделан парсинг СП

---
+++ src/main/java/ru/bjcreslin/web/Index.java
@@ -0,0 +1,14 @@
+package ru.bjcreslin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index {
+    @GetMapping({"/index", "/"})
+    public String getIndexFile() {
+        return "index";
+    }
+
+
+}
