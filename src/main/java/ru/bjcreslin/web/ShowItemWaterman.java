d89f57570d9e26b96fcacb4cd336cb7b5b3b964b
bjcreslin <bjcreslin@gmail.com>
2019-04-17 15:28:11 +0700

    Начало добавления Водяного

---
+++ src/main/java/ru/bjcreslin/web/ShowItemWaterman.java
@@ -0,0 +1,27 @@
+package ru.bjcreslin.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.bjcreslin.service.ItemSPService;
+import ru.bjcreslin.service.ItemWatermanService;
+
+/**
+ * показывает таблицу с данными СП
+ */
+@Controller
+@RequestMapping("/watermanitem")
+public class ShowItemWaterman {
+    @Autowired
+    private ItemWatermanService itemWatermanService;
+
+    @GetMapping("/showall")
+    public String action(Model model) {
+        model.addAttribute("item_waterman", itemWatermanService.findAll());
+        return "showAllWaterman";
+    }
+
+}
