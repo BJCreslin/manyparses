66c9cd6c43c440c1e69bb87813e75116013294ea
bjcreslin <bjcreslin@gmail.com>
2019-04-26 15:59:59 +0700

    Добабил метод save в Сервис класс для WatremanItem

--- src/main/java/ru/bjcreslin/DAO/ItemWatermanRepository.java
+++ src/main/java/ru/bjcreslin/DAO/ItemWatermanRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ItemWatermanRepository extends CrudRepository<ItemWatermanDAO, Long> {
      ItemWatermanDAO findByCode(Long code);
+     void save(WatermanItemDAO watermanItemDAO);
 }
--- src/main/java/ru/bjcreslin/service/WatermanItemService.java
+++ src/main/java/ru/bjcreslin/service/WatermanItemService.java
@@ -4,13 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.bjcreslin.DAO.WatermanItemDAO;
 import ru.bjcreslin.DAO.WatermanItemRepository;
+import ru.bjcreslin.model.WatermanItem;
+
+import java.util.Date;
 
 @Service
 public class WatermanItemService {
     @Autowired
     WatermanItemRepository watermanItemRepository;
 
-    public WatermanItemDAO findById(Long code){
+    public WatermanItemDAO findByCode(Long code) {
         return watermanItemRepository.findByCode(code);
+    }
+
+    public void save(WatermanItem watermanItem) {
+        WatermanItemDAO watermanItemDAO = findByCode(watermanItem.getCode());
+        if (watermanItemDAO == null) {
+            watermanItemDAO = new WatermanItemDAO();
+        }
+        watermanItemDAO.setAddress(watermanItem.getAddress());
+        watermanItemDAO.setCurrency(watermanItem.getCurrency());
+        watermanItemDAO.setGroup(watermanItem.getGroupe());
+        watermanItemDAO.setName(watermanItem.getName());
+        watermanItemDAO.setPrice(watermanItem.getPrice());
+        watermanItemDAO.setDate(new Date());
+        watermanItemRepository.save(watermanItemDAO);
+
     }
 }
