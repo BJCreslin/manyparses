ee115a211da7536a62b7c7d9866b871ab3fd4474
bjcreslin <bjcreslin@gmail.com>
2019-05-21 18:04:24 +0700

    page for analyse

--- src/main/java/ru/bjcreslin/web/Analyze.java
+++ src/main/java/ru/bjcreslin/web/Analyze.java
@@ -2,6 +2,7 @@ package ru.bjcreslin.web;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -24,27 +25,58 @@ import java.util.List;
  * todo сделать анализ
  * 1. по цене
  */
-@AllArgsConstructor
 public class Analyze {
-    AnalyzeSPServiceIMPL analyzeSPServiceIMPL;
-    AnalyzeKITServiceIMPL analyzeKITServiceIMPL;
+    public Analyze(AnalyzeSPServiceIMPL analyzeSPServiceIMPL, AnalyzeKITServiceIMPL analyzeKITServiceIMPL) {
+        this.analyzeSPServiceIMPL = analyzeSPServiceIMPL;
+        this.analyzeKITServiceIMPL = analyzeKITServiceIMPL;
+    }
+
+    private AnalyzeSPServiceIMPL analyzeSPServiceIMPL;
+    private AnalyzeKITServiceIMPL analyzeKITServiceIMPL;
+    private PagedListHolder pagedListHolder;
+    private List<Item> stroyparkItemDTOList;
 
     @GetMapping("/cheap_sp")
     public String cheapSP(Model model) {
-        List<Item> stroyparkItemDTOList = analyzeSPServiceIMPL.findAllCheaps();
-//        Page <Item> page=new PageImpl<>(stroyparkItemDTOList);
-//        Pageable pageable= PageRequest()
-
-        model.addAttribute("cheaplist", stroyparkItemDTOList);
-        model.addAttribute("name","SP");
+        stroyparkItemDTOList = analyzeSPServiceIMPL.findAllCheaps();
+        pagedListHolder = new PagedListHolder(stroyparkItemDTOList);
+        pagedListHolder.setPageSize(10);
+        pagedListHolder.setPage(0);
+        model.addAttribute("cheaplist", pagedListHolder.getPageList());
+        model.addAttribute("name", "spcheap");
         return "cheap_analizy";
     }
 
+    @GetMapping("/spcheap/next")
+    public String cheapSPnext(Model model) {
+        pagedListHolder.nextPage();
+        model.addAttribute("cheaplist", pagedListHolder.getPageList());
+        model.addAttribute("name", "spcheap");
+        return "cheap_analizy";
+    }
+
+    @GetMapping("/spcheap/first")
+    public String cheapSPfirst(Model model) {
+        pagedListHolder.setPage(0);
+        model.addAttribute("cheaplist", pagedListHolder.getPageList());
+        model.addAttribute("name", "spcheap");
+        return "cheap_analizy";
+    }
+
+    @GetMapping("/spcheap/prev")
+    public String cheapSPPrev(Model model) {
+        pagedListHolder.previousPage();
+        model.addAttribute("cheaplist", pagedListHolder.getPageList());
+        model.addAttribute("name", "spcheap");
+        return "cheap_analizy";
+    }
+
+
     @GetMapping("/cheap_kit")
     public String cheapKIT(Model model) {
-        List<KitItemDTO> itemList  = analyzeKITServiceIMPL.findAllCheaps();
+        List<KitItemDTO> itemList = analyzeKITServiceIMPL.findAllCheaps();
         model.addAttribute("cheaplist", itemList);
-        model.addAttribute("name","Kit");
+        model.addAttribute("name", "Kit");
         return "cheap_analizy";
     }
 
